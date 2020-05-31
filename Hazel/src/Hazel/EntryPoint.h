514cf84c3ca03c6366add418d5e16a601952766c
Danil Ivanov <divanov@student.ethz.ch>
2020-06-01 01:24:42 +0200

    just making sure shit works

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -7,8 +7,7 @@ extern Hazel::Application* Hazel::CreateApplication();
 int main(int argc, char** argv)
 {
 	Hazel::Log::Init();
-	HZ_CORE_WARN("Init log!");
-	HZ_INFO("Hello!");
+	HZ_CORE_INFO("Initialized log!");
 	int i = 5;
 	HZ_INFO("Hello! Var: {0}", i);
 
