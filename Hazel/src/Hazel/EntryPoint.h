f560c9fb57381aec52e2bd5133b3a775c9a582e9
Danil Ivanov <divanov@student.ethz.ch>
2020-05-31 18:46:44 +0200

    added logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Init log!");
+	HZ_INFO("Hello!");
+	int i = 5;
+	HZ_INFO("Hello! Var: {0}", i);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
