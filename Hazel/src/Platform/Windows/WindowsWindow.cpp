8b8ad1a8d7485ae880f14739f2d07e044f8ebfb4
Danil Ivanov <divanov@student.ethz.ch>
2020-06-02 21:37:32 +0200

    setup glad

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -8,6 +8,8 @@
 #include "Hazel/Events/KeyEvent.h";
 #include "Hazel/Events/MouseEvent.h";
 
+
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 namespace Hazel {
@@ -51,6 +53,8 @@ namespace Hazel {
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		HZ_CORE_ASSERT(status, "Failed to initialize Glad!");
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
